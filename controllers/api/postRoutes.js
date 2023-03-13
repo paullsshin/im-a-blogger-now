f5e9227bc7fc73add713329f46fa0c386d9b4d9a
paullsshin <paullsshin@hotmail.com>
2023-03-12 19:07:07 -0700

    controllers/routes are being worked on

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,48 +1,40 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const isAuth = require("../../utils/auth");
 
-// const router = require('express').Router();
-// const { Post } = require('../../models');
-// const isAuth = require("../../utils/auth");
+// this will render the page
+router.post('/', async (req,res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
-// //render post page
-// router.get("/", async (req, res) => {
-//   const post = { name: "", content: "" }
-//   res.render("editpost", { post, newPost: true, loggedIn: req.session.loggedIn });
-// });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// //create a post
-// router.post('/', async (req, res) => {
-//   try {
-//     const newPost = await Post.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
+// this will delete a post
+router.delete('/', isAuth, async (req,res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
 
-//     res.status(200).json(newPost);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+        if (postData) {
+            res.status(200).json(postData);
+        } else {
+            res.status(404).json({message: "your search for " + req.params.id + " could not be found in the system."});
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
-// //update a post
+module.exports = router;
 
-// //delete a post
-// router.delete('/:id',isAuth, async (req, res) => {
-//   try {
-//     const postData = await Post.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (postData) {
-//       res.status(200).json(postData);
-//     } else {
-//       res.status(404).json({ message: "post " + req.params.id + " was not found" });
-//     }
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// module.exports = router;
