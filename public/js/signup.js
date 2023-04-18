fe821ffb7800495131020dfea2f2ec0061b31f2f
paullsshin <paullsshin@hotmail.com>
2023-04-18 14:15:09 -0700

    trying to configure code to allow for signing up

---
+++ public/js/signup.js
@@ -0,0 +1,33 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        // console.log('SUCCESS ---------------------------------------------------')
+  
+        document.location.replace('/dashboard/');
+      } else {
+        console.log(response.statusText);
+        // console.log(response);
+        // console.log(response.statusText);
+  
+      }
+    }
+  }
+  
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
