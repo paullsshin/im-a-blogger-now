43628585547f0f79c207c9250678f3c0dc30875c
paullsshin <paullsshin@hotmail.com>
2023-04-18 13:10:20 -0700

    added logic for dashboardroutes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,2 +1,82 @@
 const router = require('express').Router();
-const 
+const sequelize = require('../config/connection');
+const withAuth = require ('../utils/auth');
+const { User, Post, Comment} = require('../models/');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post-content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                  }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(PostData => {
+        const posts = PostData.map(post.get({plain: true}));
+        res.render('dashboard', {posts, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'title',
+        'post_content',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(PostData => {
+        if (PostData) {
+          const post = PostData.get({ plain: true });
+          
+          res.render('edit-post', {
+            post,
+            loggedIn: true
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+  module.exports = router;
